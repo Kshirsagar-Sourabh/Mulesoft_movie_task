749fe864ef635e63d272d00f6bf3e42e41f38014
Kshirsagar-Sourabh <Kshirsagar-Sourabh>
2021-12-02 21:24:11 +0530

    first commit

---
+++ Mulesoft_task/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+c.execute("""CREATE TABLE movies (
+#             movie text,
+#             actor text,
+#             actress text,
+#             director text,
+#             year integer
+#             )""")
